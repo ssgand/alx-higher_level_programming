e6dc90ef931bb19ef1856de16c93f8e27c557dd2
ssgand <sstevegandhi@gmail.com>
2022-12-04 07:33:23 +0000

    function to remove any c from a string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    print(my_string.remove(C, c))
+    for i in my_string:
+        if i == 'C':
+            new_string = my_string.replace('C', '')
+            for i in new_string:
+                if i == 'c':
+                    new_string = new_string.replace('c', '')
+        elif i == 'c':
+            new_string = my_string.replace('c', '')
+            for i in new_string:
+                if i == 'C':
+                    new_string = new_string.replace('C', '')
+    return new_string
