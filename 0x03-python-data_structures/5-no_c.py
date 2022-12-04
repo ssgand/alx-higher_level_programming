f40233a0301f9285384f5b7e454782e709cd4271
ssgand <sstevegandhi@gmail.com>
2022-12-04 17:21:05 +0000

    Corrections 1

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    new_string = ''
     for i in my_string:
-        if i == 'C':
-            new_string = my_string.replace('C', '')
-            for i in new_string:
-                if i == 'c':
-                    new_string = new_string.replace('c', '')
-        elif i == 'c':
-            new_string = my_string.replace('c', '')
+        if i == 'c':
+            new_string += ''
             for i in new_string:
                 if i == 'C':
-                    new_string = new_string.replace('C', '')
+                    new_string += ''
+        elif i == 'C':
+            new_string += ''
+            for i in new_string:
+                if i == 'c':
+                    new_string += ''
+        else:
+            new_string += i
     return new_string
