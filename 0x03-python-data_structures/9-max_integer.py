b58312d006e354c8975db4d1cfac4fdb52057b2c
ssgand <sstevegandhi@gmail.com>
2022-12-05 16:07:29 +0000

    function to print the biggest number in a list

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    biggest = None
+    num = None
+    if my_list == []:
+        return None
+    else:
+        for i in my_list:
+            num = i
+            if biggest == None:
+                biggest = num
+            elif num > biggest:
+                biggest = num
+            else:
+                continue
+    return biggest
+
