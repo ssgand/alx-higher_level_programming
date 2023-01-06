9b9b398a730e191c1955c3f05b4ca98f85ff3bea
ssgand <sstevegandhi@gmail.com>
2023-01-06 12:37:56 +0000

    adding a new method to the square class

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+'''Defining a class.'''
+
+
+class Square:
+    '''The square class.'''
+    def __init__(self, size=0):
+        '''Initializing the square class.'''
+        self.__size = size
+
+    def area(self):
+        '''Method to calculate the area of a square.'''
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print('')
+        else:
+            print('#' * self.area())
+
+    @property
+    def size(self):
+        '''Set the size of the square.'''
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value
