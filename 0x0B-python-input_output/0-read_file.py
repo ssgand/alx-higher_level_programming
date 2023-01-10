6f3aae84682918dd5a2ef58c1989e6b2ffb052a1
ssgand <sstevegandhi@gmail.com>
2023-01-10 10:48:16 +0000

    Corrections

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,6 +5,7 @@
 
 def read_file(filename=''):
     '''Function to open a file for read_only.'''
-    with open(filename, 'r', encoding='utf-8') as fhand:
-        fread = fhand.read()
-        print(fread)
+    with open(filename, 'r', encoding='UTF8') as fhand:
+        for line in fhand:
+            # fread = fhand.read()
+            print(line, end='')
