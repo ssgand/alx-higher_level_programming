33fd3a7d7c78178da4b33a6412907fd87eecead3
ssgand <sstevegandhi@gmail.com>
2023-01-10 10:50:41 +0000

    Corrections

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,8 +2,8 @@
 
 '''Defining a new function.'''
 
+
 def write_file(filename='', text=''):
     '''Function to open and write to a file.'''
     with open(filename, 'w', encoding='utf-8') as fhand:
         return fhand.write(text)
-
