e49e9614775d6b05ba4c09683e5df00a1144ccd4
ssgand <sstevegandhi@gmail.com>
2023-01-10 11:16:35 +0000

    Function to represent a python object to json string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import json
+'''Importing the json module.'''
+
+'''Defining a new function.'''
+
+
+def to_json_string(my_obj):
+    '''Function to make python objects into javascript strings.'''
+    return json.dumps(my_obj)
